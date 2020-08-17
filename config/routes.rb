7c31a4ae95fb68e543628ac4bbc00e3e6f7dc23f
vagrant <vagrant@localhost.localdomain>
2020-08-17 03:00:22 +0000

    [Finish] favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
   root 'home#top'
   get 'home/about'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
